972404476869c24b6dd2333ce271cb5032a2fa18
jaspe <jasperfue@live.de>
2023-08-06 21:18:53 +0200

    ohne socket.io

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const helmet = require('helmet');
 const {Server} = require('socket.io');
 
 const server = require('http').createServer(app);
-const io = new Server(server);
+//const io = new Server(server);
 
 app.use(helmet());
 
 app.use(express.json());
 
-io.engine.use(helmet());
+//io.engine.use(helmet());
 
 app.get("/", (req, res) => res.send("<h1> Hello World</h1>"));
 
